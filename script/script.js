1e63a1a3e47f26f4a4328e04aac984dc7da2a518
Manohar <manohargella272@gmail.com>
2024-11-11 22:48:55 +0530

    let

--- script/script.js
+++ script/script.js
@@ -3,9 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Array to hold multiple link groups for dynamic creation
     const linkGroups = [
-        { title: 'Fuck list', links: ['Link One', 'Link Two', 'Link Three', 'Link Four', 'Link Five'] },
-        { title: 'Category Title', links: ['Link One', 'Link Two', 'Link Three', 'Link Four', 'Link Five'] },
-        { title: 'Category Title', links: ['Link One', 'Link Two', 'Link Three', 'Link Four', 'Link Five'] },
+        { title: 'WarmUp frm CH - 2', links: ['Link One', 'Link Two', 'Link Three', 'Link Four', 'Link Five'] },
+        { title: 'Week - 2', links: ['Link One', 'Link Two', 'Link Three', 'Link Four', 'Link Five'] },
+        { title: 'Week - 2 Off', links: ['Link One', 'Link Two', 'Link Three', 'Link Four', 'Link Five'] },
+        { title: 'DOM', links: ['Link One', 'Link Two', 'Link Three', 'Link Four', 'Link Five'] },
+        { title: 'NodeJs and HTTP', links: ['Link One', 'Link Two', 'Link Three', 'Link Four', 'Link Five'] },
+        { title: 'Week - 4 Off', links: ['Link One', 'Link Two', 'Link Three', 'Link Four', 'Link Five'] },
+        { title: 'Week - 5', links: ['Link One', 'Link Two', 'Link f', 'Link Four', 'Link Five'] },
         // Add more groups as needed
     ];
 
